5090bccfdd781ddfa47ca07a94192e5476d1ad04
Neverson Kawana <107488047+itachithesixpathsage@users.noreply.github.com>
2024-12-22 23:57:51 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://fastgrab_user_one:NZMKgSX7FsS2PfLf@cluster0.owirn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(()=>console.log("DB Connected"));
+    await mongoose.connect("mongodb+srv://").then(()=>console.log("DB Connected"));
 }
